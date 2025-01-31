649eb29b756c7a561de85436edc38cd23fbc8742
Akshaya Parida <akshayaparida2811@gmail.com>
2025-02-01 01:29:35 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",  // Update this to include your app folder
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./styles/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
